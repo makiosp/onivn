5f074426e53470526344fc67b988d9689dcf56a5
makiosp <thiendepzai1997@gmail.com>
2017-09-26 15:59:59 -0700

    Update get.php

--- get.php
+++ get.php
@@ -121,13 +121,3 @@ provider: "http",
 } 
 ?> 
 
- 
-
-<?php
-	// We're done! Save the cached content to a file
-	$fp = fopen($cachefile, 'w');
-	fwrite($fp, ob_get_contents());
-	fclose($fp);
-	// finally send browser output
-	ob_end_flush();
-?>
